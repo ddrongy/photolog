d5af0c623d800a5973d3e89abdde97c1afa07be0
ddrongy <ddrongy@gmail.com>
2023-07-14 14:04:11 +0900

    coordinate 오차범위 수정

--- src/main/java/photolog/api/domain/Coordinate.java
+++ src/main/java/photolog/api/domain/Coordinate.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter
 @NoArgsConstructor
@@ -14,5 +16,19 @@ public class Coordinate {
     public Coordinate(Double longitude, Double latitude) {
         this.longitude = longitude;
         this.latitude = latitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return Math.abs(that.latitude - latitude) < 0.001 &&
+                Math.abs(that.longitude - longitude) < 0.001;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Math.round(latitude * 1000), Math.round(longitude * 1000));
     }
 }
