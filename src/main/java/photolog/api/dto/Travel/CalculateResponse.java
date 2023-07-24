7ccad056a1947e6f4ea4d78e1ecd7505b245d934
ddrongy <ddrongy@gmail.com>
2023-07-25 01:15:08 +0900

    cal return 수정

--- src/main/java/photolog/api/dto/Travel/CalculateResponse.java
+++ src/main/java/photolog/api/dto/Travel/CalculateResponse.java
@@ -2,12 +2,14 @@ package photolog.api.dto.Travel;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CalculateResponse {
     private Integer night;
     private Integer day;
@@ -15,5 +17,7 @@ public class CalculateResponse {
     private LocalDate endDate;
     private Integer locationNum;
     private Integer photoNum;
-    private List<Long> locationList;
+    private List<Long> locationIdList;
+    private List<String> locationAddress;
+    private List<String> locationImg;
 }
