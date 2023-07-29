91dcc72685bf120c90cdaf414909c1335dd1ef9c
ddrongy <ddrongy@gmail.com>
2023-07-29 18:06:13 +0900

    fix: tag unique remove

--- src/main/java/photolog/api/domain/HashTag.java
+++ src/main/java/photolog/api/domain/HashTag.java
@@ -20,7 +20,7 @@ public class HashTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String tag;
 
     @ManyToOne(fetch = FetchType.LAZY)
