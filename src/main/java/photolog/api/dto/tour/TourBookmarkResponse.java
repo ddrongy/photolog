55071a954922ffd1b32e3255119c8237cf9137ec
ddrongy <ddrongy@gmail.com>
2023-08-08 14:55:55 +0900

    fix: tourbookmark response 수정

--- src/main/java/photolog/api/dto/tour/TourBookmarkResponse.java
+++ src/main/java/photolog/api/dto/tour/TourBookmarkResponse.java
@@ -10,10 +10,12 @@ import photolog.api.domain.Tour;
 @AllArgsConstructor
 public class TourBookmarkResponse {
     private Long tourId;
+    private Long contentId;
     private String firstImage;
 
     public TourBookmarkResponse(Tour tour) {
         this.tourId = tour.getId();
+        this.contentId = tour.getContentId();
         this.firstImage = tour.getFirstimage();
     }
 
